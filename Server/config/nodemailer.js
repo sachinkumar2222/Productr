8ea7f9a3bbf11a1ad2eab3dfae1694cf0521695e
sachin_sparky <sachin2322006@gmail.com>
2026-02-03 05:01:54 +0530

    Refactor nodemailer.js to use ES6 imports

--- Server/config/nodemailer.js
+++ Server/config/nodemailer.js
@@ -1,25 +1,12 @@
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
-
-// Verify connection configuration
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log("Transporter verification error:", error);
-    } else {
-        console.log("Server is ready to take our messages");
-    }
-});
-
-module.exports = transporter;

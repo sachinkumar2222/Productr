40f6370746c77e209a6148049ddbe302abbdbf64
sachin_sparky <sachin2322006@gmail.com>
2026-02-03 10:18:35 +0530

    Update nodemailer configuration for SendGrid

--- Server/config/nodemailer.js
+++ Server/config/nodemailer.js
@@ -1,19 +1,13 @@
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    logger: true,
-    debug: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: 'smtp.sendgrid.net',
+  port: 587,
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY
+  }
 });
 
 module.exports = transporter;
